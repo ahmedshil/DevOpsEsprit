a5ac6ad2eef926134545dca811a4f98de678f07a
ahmedshil <ahmed.shil@esprit.tn>
2021-11-16 11:37:33 +0100

    done

---
+++ src/main/java/tn/esprit/spring/controller/RestControlEntreprise.java
@@ -0,0 +1,89 @@
+package tn.esprit.spring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import tn.esprit.spring.entities.Departement;
+import tn.esprit.spring.entities.Departements;
+import tn.esprit.spring.entities.Entreprise;
+import tn.esprit.spring.entities.Entreprises;
+import tn.esprit.spring.services.IEmployeService;
+import tn.esprit.spring.services.IEntrepriseService;
+import tn.esprit.spring.services.ITimesheetService;
+
+@RestController
+public class RestControlEntreprise {
+
+	
+	@Autowired
+	IEmployeService iemployeservice;
+	@Autowired
+	IEntrepriseService ientrepriseservice;
+	@Autowired
+	ITimesheetService itimesheetservice;
+	
+	@PostMapping("/ajouterEntreprise")
+	@ResponseBody
+	public int ajouterEntreprise(Entreprises ssiiConsulting) {
+		Entreprise persistantEntreprise = new Entreprise();
+		persistantEntreprise.setName(ssiiConsulting.getName());
+		persistantEntreprise.setRaisonSocial(ssiiConsulting.getRaisonSocial());
+		ientrepriseservice.ajouterEntreprise(persistantEntreprise);
+		return persistantEntreprise.getId();
+	}
+	
+	// http://localhost:8081/SpringMVC/servlet/affecterDepartementAEntreprise/1/1
+    @PutMapping(value = "/affecterDepartementAEntreprise/{iddept}/{identreprise}") 
+	public void affecterDepartementAEntreprise(@PathVariable("iddept")int depId, @PathVariable("identreprise")int entrepriseId) {
+		ientrepriseservice.affecterDepartementAEntreprise(depId, entrepriseId);
+	}
+    
+    // http://localhost:8081/SpringMVC/servlet/deleteEntrepriseById/1
+    @DeleteMapping("/deleteEntrepriseById/{identreprise}") 
+	@ResponseBody 
+	public void deleteEntrepriseById(@PathVariable("identreprise")int entrepriseId)
+	{
+		ientrepriseservice.deleteEntrepriseById(entrepriseId);
+	}
+    
+    // http://localhost:8081/SpringMVC/servlet/getEntrepriseById/1
+    @GetMapping(value = "getEntrepriseById/{identreprise}")
+    @ResponseBody
+	public Entreprise getEntrepriseById(@PathVariable("identreprise") int entrepriseId) {
+
+		return ientrepriseservice.getEntrepriseById(entrepriseId);
+	}
+    
+    // http://localhost:8081/SpringMVC/servlet/ajouterDepartement
+ 
+
+ 	@PostMapping("/ajouterDepartement")
+	public int ajouterDepartement(Departements dep) {
+ 		Departement persistantDep= new Departement();
+ 		persistantDep.setName(dep.getName());
+		return ientrepriseservice.ajouterDepartement(persistantDep);
+	}
+	
+ 	 // http://localhost:8081/SpringMVC/servlet/getAllDepartementsNamesByEntreprise/1
+    @GetMapping(value = "getAllDepartementsNamesByEntreprise/{identreprise}")
+    @ResponseBody
+	public List<String> getAllDepartementsNamesByEntreprise(@PathVariable("identreprise") int entrepriseId) {
+		return ientrepriseservice.getAllDepartementsNamesByEntreprise(entrepriseId);
+	}
+
+    // URL : http://localhost:8081/SpringMVC/servlet/deleteDepartementById/3
+    @DeleteMapping("/deleteDepartementById/{iddept}") 
+	@ResponseBody 
+	public void deleteDepartementById(@PathVariable("iddept") int depId) {
+		ientrepriseservice.deleteDepartementById(depId);
+
+	}
+}
